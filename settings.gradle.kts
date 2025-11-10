b3ed4a1d3cd9dd8e1f06d3180548ae445c99b358
ilabeu <mionerandom55l@gmail.com>
2025-11-09 22:29:04 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,7 @@
 pluginManagement {
     repositories {
-        maven {
-            name = "Fabric"
-            url = uri("https://maven.fabricmc.net/")
-        }
-        mavenCentral()
+        maven("https://maven.fabricmc.net/")
         gradlePluginPortal()
+        mavenCentral()
     }
 }
